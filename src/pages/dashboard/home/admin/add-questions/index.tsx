181d64fcb229128771761f6590467bcc5bb75e35
diwasdhungana <diwasdhungana@gmail.com>
2024-11-01 16:52:31 +0545

    disabled Next Gen

--- src/pages/dashboard/home/admin/add-questions/index.tsx
+++ src/pages/dashboard/home/admin/add-questions/index.tsx
@@ -67,7 +67,7 @@ const addQuestions = () => {
                         value={gen.value}
                         label={gen.label}
                         checked={selectedGen === gen.value}
-                        // disabled={gen.value === 'nextgen'}
+                        disabled={gen.value === 'nextgen'}
                       />
                     );
                   })}
